dab0f95e465be91c0c7d6cee02c3c0008260f6f2
Abhishek Dasare <107669940+AbhishekDasare@users.noreply.github.com>
2026-06-05 00:06:18 +0530

    Add Kafka consumer to read messages from sales_topic

---
+++ scripts/consumer.py
@@ -0,0 +1,11 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer(
+    'sales_topic',
+    bootstrap_servers='localhost:9092',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+for msg in consumer:
+    print(msg.value)
